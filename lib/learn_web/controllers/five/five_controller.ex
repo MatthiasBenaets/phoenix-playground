c02b1c1041f928ca70dd4e6d3be321600295295d
MatthiasBenaets <matthias.benaets@gmail.com>
2025-02-18 22:00:14 +0100

    function clause matching

--- lib/learn_web/controllers/five/five_controller.ex
+++ lib/learn_web/controllers/five/five_controller.ex
@@ -7,10 +7,21 @@ defmodule LearnWeb.FiveController do
   end
 
   # function for route /five/:id
-  # for the parameters it pattern matches and looks for id. the value is store under variable id and is assigned
+  # for the parameters it pattern matches and looks for id. the value is store under variable id and query and is assigned
+  # so localhost:4000/five/1?number=2 will use this function
+  def number(conn, %{"id" => id, "number" => query}) do
+    conn
+    |> assign(:id, id)
+    |> assign(:query, query)
+    |> render(:number)
+  end
+
+  # fallback if only id is present
+  # so localhost:4000/five/1 will use this function
   def number(conn, %{"id" => id}) do
     conn
     |> assign(:id, id)
+    |> assign(:query, nil)
     |> render(:number)
   end
 end
